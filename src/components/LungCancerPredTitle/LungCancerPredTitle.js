17a5dec6415693d2d04223beeb8ddff7f8c56df6
Nazhim <nazhimkalam@gmail.com>
2021-01-13 16:31:46 +0530

    created components

---
+++ src/components/LungCancerPredTitle/LungCancerPredTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LungCancerPredTitle() {
+    return (
+        <div>
+            <h2>Lung Cancer Prediction</h2>
+			<p>How it works - Just fill all the fields and hit the predict button</p>
+        </div>
+    )
+}
+
+export default LungCancerPredTitle
---
+++ src/components/MainTitle/MainTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainTitle() {
+	return (
+		<div className="mainTitle">
+			<h1>Lung Cancer</h1>
+		</div>
+	);
+}
+
+export default MainTitle;
